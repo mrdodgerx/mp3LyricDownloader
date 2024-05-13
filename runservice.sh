062a2ca05e63e0f1669e7f78cf3324a68d4d4915
MrDodgerX <dodgerxc47@gmail.com>
2024-05-13 09:44:34 +0800

    add to gitignore

--- runservice.sh
+++ runservice.sh
@@ -1,2 +1,3 @@
-source venv/bin/activate
-python main.py
+cd /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader
+source /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/venv/bin/activate
+/home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/venv/bin/python /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/main.py
