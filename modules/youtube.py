867fb3bd9da721182a1e997b79f1f0450aef781c
MrDodgerX <dodgerxc47@gmail.com>
2024-05-14 14:19:20 +0800

    fix mp3 audio quality

--- modules/youtube.py
+++ modules/youtube.py
@@ -1,5 +1,6 @@
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
+from pydub import AudioSegment
 import os
 from configparser import ConfigParser
 from modules.google import send_notification
@@ -24,25 +25,39 @@ class YoutubeMp3():
 
     def download_mp3(self):
         if self.video_link:
-            yt = YouTube(self.video_link)
-            video_title = self.song_title.strip()
-            audio_stream = yt.streams.filter(only_audio=True).first()
+            try:
+                # Create output directory if it doesn't exist
+                if not os.path.exists(MP3_OUTPUT):
+                    os.makedirs(MP3_OUTPUT)
 
-            if audio_stream:
-                mp3_file = f'{video_title}.mp3'
-                if not os.path.exists(os.path.join(MP3_OUTPUT, mp3_file)):
-                    out_file = audio_stream.download(output_path=MP3_OUTPUT, filename=video_title)
-                    # save the file with .mp3 extension
-                    base, ext = os.path.splitext(out_file)
-                    new_file = base + '.mp3'
-                    os.rename(out_file, new_file)
-                    print(f'{video_title} has been successfully downloaded.')
+                # Define the MP3 path with video_title.mp3
+                video_title = self.song_title.strip()
+                mp3_path = os.path.join(MP3_OUTPUT, f"{video_title}.mp3")
+
+                # Check if the file already exists
+                if os.path.exists(mp3_path):
+                    print(f"File '{mp3_path}' already exists. Skipping download.")
+                    return
+
+                # Download the YouTube video
+                yt = YouTube(self.video_link)
+                audio_stream = yt.streams.filter(only_audio=True).first()
+
+                if audio_stream:
+                    # Set the download path
+                    video_download_path = audio_stream.download(output_path=MP3_OUTPUT)
+
+                    # Convert to MP3
+                    AudioSegment.from_file(video_download_path).export(mp3_path, format='mp3')
+
+                    # Optionally, remove the original file
+                    os.remove(video_download_path)
+
+                    print(f'Successfully downloaded and converted to {mp3_path}')
                     send_notification('Song is Downloaded', video_title)
 
-                else:
-                    print(f'{mp3_file} already exists in the folder. Skipping download.')
-            else:
-                print('No audio stream available for download.')
+            except Exception as e:
+                print(f'An error occurred: {e}')
     
     def run(self):
         self.youtube_search()
