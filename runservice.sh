d7b57a6b7a180071957ebb01e0ff0daa40656d0f
MrDodgerX <dodgerxc47@gmail.com>
2024-05-14 15:40:08 +0800

    remove musicmatch file

--- runservice.sh
+++
@@ -1,3 +0,0 @@
-cd /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader
-source /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/venv/bin/activate
-/home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/venv/bin/python /home/mrdodgerx/C0d34L1F3/mp3LyricDownloader/main.py
